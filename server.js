1a56999cc625b6c7e376a1ef4463af6acea09126
dennis-22-csc <dennisakpotaire@gmail.com>
2025-11-20 23:03:49 +0100

    api fix

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const corsOptions = {
     if (!origin) return callback(null, true);
     
     const allowedOrigins = [
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
+      'http://localhost:8000',
+      'http://127.0.0.1:8000',
       process.env.FRONTEND_URL
     ].filter(Boolean); // Remove any undefined values
 
